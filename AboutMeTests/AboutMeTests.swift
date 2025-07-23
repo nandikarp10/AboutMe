ab7535aa346df26e40114439ef182b4efa33fee6
Nandika Peddapuram <nandikarp10@gmail.com>
2025-07-23 14:56:31 -0500

    Initial Commit

---
+++ AboutMeTests/AboutMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeTests.swift
+//  AboutMeTests
+//
+//  Created by Nandika P on 7/23/25.
+//
+
+import Testing
+@testable import AboutMe
+
+struct AboutMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
