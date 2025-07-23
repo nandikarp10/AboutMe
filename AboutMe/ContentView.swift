b6c812359535279a8648c48c927581d8d253cd24
Nandika Peddapuram <nandikarp10@gmail.com>
2025-07-23 15:38:36 -0500

    Created AboutMe Project

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,13 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(.systemPink)
+                .ignoresSafeArea()
+            VStack(alignment: .center, spacing: 50) {
+                
+                Image("self")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(5)
+            
+                
+                HStack(spacing: 10.0)
+                {
+                    Text("Nandika Peddapuram")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Text("A Passionate Artist!")
+                }
+                
+                Image("art")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(5)
+            
+                
+                Text ("I have been an artist since elementary school and is something I look forward to doing everyday!")
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white))
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .padding()
         }
-        .padding()
     }
 }
 
